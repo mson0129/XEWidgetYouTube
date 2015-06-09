ebdc65f0e667d8ff55cd8e003f94a4f4c81686a2
Michael Son <mson0129@gmail.com>
2015-06-10 08:01:41 +0900

    1.0.1.
    Bug fixed.

--- youtube.class.php
+++ youtube.class.php
@@ -4,6 +4,7 @@
 //Youtube
 //Studio2b(www.studio2b.kr)
 //Michael Son(mson0129@gmail.com)
+//10JUL2015(1.0.0.) - This file is newly created.
 class youtube extends WidgetHandler {
 	function proc($args) {
 		//xFacility2014 - including the part of frameworks
@@ -17,55 +18,52 @@ class youtube extends WidgetHandler {
 		
 		//Get settings
 		$widgetInfo = new stdClass();
-
-		if(isset($args->api_key)) {
-			if(isset($args->mid_id)) {
-				$obj->module_srl = $args->mid_id;
-				$result = executeQueryArray('widgets.youtube.getModule', $obj);
-				if(!is_null($result->data[0]->mid))
-					$mid = $result->data[0]->mid;
-				unset($result);
-				$result = executeQueryArray('widgets.youtube.getModuleValues', $obj);
-				foreach($result->data as $val) {
-					if($val->name=="api_key")
-						$apiKey = $val->value;
-					else if($val->name=="playlist_id")
-						$playlistId = $val->value;
-					else if($val->name=="list_count")
-						$items = $val->value;
-				}
+		
+		if(isset($args->mid_id)) {
+			$obj->module_srl = $args->mid_id;
+			$result = executeQueryArray('widgets.youtube.getModule', $obj);
+			if(!is_null($result->data[0]->mid))
+				$mid = $result->data[0]->mid;
+			unset($result);
+			$result = executeQueryArray('widgets.youtube.getModuleValues', $obj);
+			foreach($result->data as $val) {
+				if($val->name=="api_key")
+					$apiKey = $val->value;
+				else if($val->name=="playlist_id")
+					$playlistId = $val->value;
+				else if($val->name=="list_count")
+					$items = $val->value;
 			}
-			
-			if(!is_null($args->api_key))
-				$apiKey = $args->api_key;
-			if(!is_null($args->playlist_id))
-				$playlistId = $args->playlist_id;
-			if(!is_null($args->items) && is_numeric($args->items))
-				$items = $args->items;
-			if(!is_null($mid)) {
-				$urlBase = getNotEncodedUrl("", "mid", $mid)."&video_id=";
-				Context::set("outlink", false);
-			} else {
-				$urlBase = "//www.youtube.com/watch?v=";
-			}
-			
-			$youtube = New XFYoutube(NULL, trim($apiKey));
-			
-			//Playlist Default Value Setting
-			if(!isset($playlistId))
-				$playlistId = "PLmtapKaZsgZt3g_uAPJbsMWdkVsznn_2R"; //실시간 인기 동영상 - 한국
-			
-			//Get videos
-			$videos = $youtube->getPlaylistItems($playlistId, is_numeric($items) ? $items : 20);
-			//var_dump($videos);
-			unset($videos[totalPages], $videos[totalVideos]);
-			
-			foreach($videos as $key=>$val) {
-				$videos[$key][url] = $urlBase.$val[snippet][resourceId][videoId];
-			}
-			Context::set("videos", $videos);
-		} else
-			Context::set("error", "API Key");
+		}
+		
+		if(!is_null($args->api_key) && trim($args->api_key)=="")
+			$apiKey = $args->api_key;
+		if(!is_null($args->playlist_id) && trim($args->playlist_id)=="")
+			$playlistId = $args->playlist_id;
+		if(!is_null($args->items) && is_numeric($args->items))
+			$items = $args->items;
+		if(!is_null($mid)) {
+			$urlBase = getNotEncodedUrl("", "mid", $mid)."&video_id=";
+			Context::set("outlink", false);
+		} else {
+			$urlBase = "//www.youtube.com/watch?v=";
+		}
+		
+		$youtube = New XFYoutube(NULL, trim($apiKey));
+		
+		//Playlist Default Value Setting
+		if(is_null($playlistId))
+			$playlistId = "PLmtapKaZsgZt3g_uAPJbsMWdkVsznn_2R"; //실시간 인기 동영상 - 한국
+		
+		//Get videos
+		$videos = $youtube->getPlaylistItems($playlistId, is_numeric($items) ? $items : 20);
+		//var_dump($videos);
+		unset($videos[totalPages], $videos[totalVideos]);
+		
+		foreach($videos as $key=>$val) {
+			$videos[$key][url] = $urlBase.$val[snippet][resourceId][videoId];
+		}
+		Context::set("videos", $videos);
 		
 		$tplPath = sprintf("%sskins/%s/", $this->widget_path, (!is_null($args->skin) &&  $args->skin!="" && is_dir(sprintf("%sskins/%s/", $this->widget_path, $args->skin))) ? $args->skin : "default");
 		$tplFile = "browse";
